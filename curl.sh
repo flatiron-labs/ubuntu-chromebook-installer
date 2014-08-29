5522cd10bd7e5bbf7cecd2f5b2be556dfdebee1d
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-29 13:33:02 -0400

    fixes error in spelling of tarball file name

--- curl.sh
+++ curl.sh
@@ -2,6 +2,6 @@ mkdir $HOME/chrubuntu-installer
 cd $HOME/chrubuntu-installer
 
 wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/master.tar.gz
-tar xzf ubuntu-chromebook-installer-master.tar.gz
+tar xzf master.tar.gz
 cd ubuntu-chromebook-installer-master
 sudo bash main.sh
