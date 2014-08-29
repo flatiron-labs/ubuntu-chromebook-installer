9a90449239815236ab23208e22c791e28202a8d8
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-29 13:29:15 -0400

    switches to wgetting a tarball

--- curl.sh
+++ curl.sh
@@ -1,8 +1,7 @@
 mkdir $HOME/chrubuntu-installer
 cd $HOME/chrubuntu-installer
 
-curl https://codeload.github.com/flatiron-labs/ubuntu-chromebook-installer/archive/master.zip
-unzip ubuntu-chromebook-installer-master.zip
+wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/master.tar.gz
+tar xzf ubuntu-chromebook-installer-master.tar.gz
+cd ubuntu-chromebook-installer-master
 sudo bash main.sh
-
-rm -rf $HOME/chrubuntu-installer
--- profiles/devices/acer-c720/scripts/xcodestarter.sh
+++ profiles/devices/acer-c720/scripts/xcodestarter.sh
@@ -76,7 +76,9 @@ mkdir /etc/chromebook-environmentalizer/
 touch /etc/chromebook-environmentalizer/setup.sh
 chmod -R 777 /etc/chromebook-environmentalizer
 echo -e "gnome-terminal -x sh -c 'curl -Lo- https://raw.githubusercontent.com/flatiron-labs/chromebook-environmentalizer/master/bootstrap.sh | bash'\n\
-sudo echo -e 'Hidden=true' >> /usr/share/upstart/xdg/autostart/chromebook-environmentalizer.desktop
+sudo echo -e 'Hidden=true' >> /usr/share/upstart/xdg/autostart/chromebook-environmentalizer.desktop\n\
+sudo rm -rf /etc/chromebook-environmentalizer\n\
+sudo rm /usr/share/upstart/xdg/autostart/chromebook-environmentalizer.desktop
 " > /etc/chromebook-environmentalizer/setup.sh
 touch "chromebook-environmentalizer-script.done"
 
