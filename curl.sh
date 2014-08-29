cccc34665121571237e93a049cb46bd25ee74579
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-29 15:51:42 -0400

    changes where the tarball comes from:

--- curl.sh
+++ curl.sh
@@ -3,7 +3,7 @@ mkdir /var/chrubuntu-installer
 cd /var/chrubuntu-installer
 sudo chmod 755 /var
 
-wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/master.tar.gz
+wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/curl.tar.gz
 tar xzf master.tar.gz
 cd ubuntu-chromebook-installer-master
 sudo bash main.sh
