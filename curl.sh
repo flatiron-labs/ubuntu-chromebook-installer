b36d968d397c621c8a0f895e5668f6a6e9219135
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-08-29 15:53:52 -0400

    changes file names to curl

--- curl.sh
+++ curl.sh
@@ -4,6 +4,6 @@ cd /var/chrubuntu-installer
 sudo chmod 755 /var
 
 wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/curl.tar.gz
-tar xzf master.tar.gz
-cd ubuntu-chromebook-installer-master
+tar xzf curl.tar.gz
+cd ubuntu-chromebook-installer-curl
 sudo bash main.sh
