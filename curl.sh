c88e98e21c84b9c027326b90a149fe967ad85afe
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-02 13:29:15 -0400

    adds conditional to check if dir exists

--- curl.sh
+++ curl.sh
@@ -1,7 +1,9 @@
-sudo chmod 777 /var
-mkdir /var/chrubuntu-installer
+if [[ ! -d /var/chrubuntu-installer ]]; then
+  sudo chmod 777 /var
+  mkdir /var/chrubuntu-installer
+  sudo chmod 755 /var
+fi
 cd /var/chrubuntu-installer
-sudo chmod 755 /var
 
 wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/new-curl.tar.gz
 tar xzf new-curl.tar.gz
