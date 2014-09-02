b1461cc4d5f3d535cbae4fc982a6e70dbcc8eafe
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-02 11:51:08 -0400

    adds back l flag and rms curl.sh

--- curl.sh
+++ curl.sh
@@ -6,4 +6,5 @@ sudo chmod 755 /var
 wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/test.tar.gz
 tar xzf test.tar.gz
 cd ubuntu-chromebook-installer-test
+rm curl.sh
 sudo bash main.sh
