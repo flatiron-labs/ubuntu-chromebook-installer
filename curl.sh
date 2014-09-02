50f624f45b4795d83998a67e03076c3f6b79d965
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-02 06:58:43 -0700

    gets tarball from codestarter

--- curl.sh
+++ curl.sh
@@ -3,7 +3,7 @@ mkdir /var/chrubuntu-installer
 cd /var/chrubuntu-installer
 sudo chmod 755 /var
 
-wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/curl.tar.gz
-tar xzf curl.tar.gz
-cd ubuntu-chromebook-installer-curl
+wget https://github.com/codestarterorg/ubuntu-chromebook-installer/archive/master.tar.gz
+tar xzf master.tar.gz
+cd ubuntu-chromebook-installer-master
 sudo bash main.sh
