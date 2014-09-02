e5c05c2ec5c5ae30d6c4cfc728ddb718b49dddde
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-02 13:37:57 -0400

    adds check to see if tarball is dled

--- curl.sh
+++ curl.sh
@@ -3,9 +3,13 @@ if [[ ! -d /var/chrubuntu-installer ]]; then
   mkdir /var/chrubuntu-installer
   sudo chmod 755 /var
 fi
+
 cd /var/chrubuntu-installer
 
-wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/new-curl.tar.gz
-tar xzf new-curl.tar.gz
+if [[ ! -f new-curl.tar.gz ]]
+  wget https://github.com/flatiron-labs/ubuntu-chromebook-installer/archive/new-curl.tar.gz
+  tar xzf new-curl.tar.gz
+fi
+
 cd ubuntu-chromebook-installer-new-curl
 sudo bash -i main.sh
